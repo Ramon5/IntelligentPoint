be1afef575862898710b226ca7c5dded81b0f99b
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2020-06-14 01:08:35 -0300

    first commit

---
+++ src/main/java/com/uaicom/intelligentpointapi/IntelligentPointApplication.java
@@ -0,0 +1,13 @@
+package com.uaicom.intelligentpointapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uaicom/intelligentpointapi/IntelligentPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uaicom.intelligentpointapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
